d176929ffee7e20bb88ff74c47f4691a2c97b958
nujhat555 <nmc9486@nyu.edu>
2026-09-12 22:58:49 -0400

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -27,10 +27,10 @@ def welcome_assignment_answers(question):
         answer = "No"
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 4
+        answer = 5
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 2
+        answer = 3
 
     else:
         return "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
